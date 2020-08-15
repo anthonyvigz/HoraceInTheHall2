959f7ce8aa26a6c115e58b53c2ae6e833b9cd4a1
anthonyvigz <anthonyvigz@gmail.com>
2020-08-15 10:59:55 -0400

    setting up website

--- horace/src/App.js
+++ horace/src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./App.css";
+import video from "./files/video1.mp4";
 
 function App() {
-  return <div className="App">hey</div>;
+  return (
+    <div className="v-header container">
+      <video src={video} autoplay="true" loop="true"></video>
+      <h1>hey</h1>
+    </div>
+  );
 }
 
 export default App;
